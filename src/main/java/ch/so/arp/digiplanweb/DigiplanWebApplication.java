4875f4115f3295157e545b4dbd2a432168177d39
Stefan Ziegler <edi.gonzales@gmail.com>
2022-04-03 12:21:39 +0200

    initial commit

---
+++ src/main/java/ch/so/arp/digiplanweb/DigiplanWebApplication.java
@@ -0,0 +1,13 @@
+package ch.so.arp.digiplanweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiplanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiplanWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/arp/digiplanweb/DigiplanWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.arp.digiplanweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiplanWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
